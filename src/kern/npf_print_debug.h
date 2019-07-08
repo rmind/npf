af019dfd7a108aa2006d8aa83085058f7bf79e4c
Alexander Kiselev <kiselev99@gmail.com>
2019-07-08 17:18:36 +0300

    add debug print routines

---
+++ src/kern/npf_print_debug.h
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   npf_print_debug.h
+ * Author: alexk
+ *
+ * Created on June 21, 2016, 1:40 AM
+ */
+
+#ifndef NPF_DEBUG_H
+#define NPF_DEBUG_H
+
+#include <stdint.h>
+
+#ifdef NPF_PRINT_DEBUG
+	
+/* debug print contexts */
+enum npf_print_debug_context {
+	NPF_DC_PPTP_ALG = 0,
+	NPF_DC_GRE,
+	NPF_DC_ESTABL_CON
+};
+
+int
+npf_dprintfc(uint32_t context, char *format, ...);
+#define NPF_DPRINTFC(context, ...) npf_dprintfc(context, __VA_ARGS__)
+
+int
+npf_dprintfcl(uint32_t context, uint32_t level, char *format, ...);
+#define NPF_DPRINTFCL(context, level, ...) \
+		  npf_dprintfcl(context, level, __VA_ARGS__)
+
+void
+npf_hex_dump(const char *desc, const void *addr, int len);
+
+void
+npf_dhexdumpcl(uint32_t context, uint32_t level, char *desc, void *addr,
+		  int len);
+#define NPF_HEX_DUMPCL(context, level, desc, addr, len) \
+	npf_dhexdumpcl(context, level, desc, addr, len)
+	
+#else /* NPF_PRINT_DEBUG */
+
+#define NPF_DPRINTFC(...)
+#define NPF_DPRINTFCL(...)
+#define NPF_HEX_DUMPCL(...)
+
+#endif /* NPF_PRINT_DEBUG */
+
+#endif /* NPF_DEBUG_H */
+
