83621f8a8711f79fbe16be02436efaf0293b565b
Mindaugas Rasiukevicius <rmind@noxt.eu>
2019-07-25 01:49:50 +0100

    npfctl: fix the bpf.jit parameter handling.

--- src/npfctl/npf_build.c
+++ src/npfctl/npf_build.c
@@ -969,6 +969,7 @@ npfctl_setparam(const char *name, int val)
 {
 	if (strcmp(name, "bpf.jit") == 0) {
 		npfctl_bpfjit(val != 0);
+		return;
 	}
 	if (npf_param_set(npf_conf, name, val) != 0) {
 		yyerror("invalid parameter `%s` or its value", name);
